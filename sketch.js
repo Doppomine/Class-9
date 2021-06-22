fb9c18d7b480954124eab08d88708a984e3ec8d4
Doppomine <86176243+Doppomine@users.noreply.github.com>
2021-06-21 19:10:18 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,68 +1,37 @@
-var n1,n2;
-var b1,b2,b3,b4;
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
 
 function setup() {
-  
-  createCanvas(400, 400);
-  
-  num1 = createInput();
-  num1.position(5, 60);
-  
-  num2 = createInput();
-  num2.position(200, 60);
-  
-  b1 = createButton("ADD");
-  b1.position(10, 200);
-  b1.mousePressed(add);
-  
-  b2 = createButton("Multiply");
-  b2.position(100, 200);
-  b2.mousePressed(mul);
-  
-  b3 = createButton("Divide");
-  b3.position(200, 200);
-  b3.mousePressed(div);
-  
-  b4 = createButton("Subtraction");
-  b4.position(300, 200);
-  b4.mousePressed(sub);
-  
-  textAlign(CENTER);
-  textSize(15);
+  createCanvas(400, 400);  
+
+  btn_red = createButton("RED"); 
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
 }
+
+function red_bg()
+{
+  r = 255;
+  g = 0;
+  b = 0;
+}
+
+function green_bg()
+{
+  r = 0;
+  g = 255;
+  b = 0;
+}
+
 
 function draw() {
-  background(200,10,200)
-  text("Number 1",70,50)
-  text("Number 2",270,50)
-  
-
-}
-
-function add() { 
-  n1 =parseInt(num1.value());
-  n2 =parseInt(num2.value());
-  var n3 = n1+n2 
-  console.log(n3);
-}
-
-function mul() { 
-  n1 =parseInt(num1.value());
-  n2 =parseInt(num2.value());
-  var n3 = n1*n2 
-  console.log(n3);
-}
-
-function div() { 
-  n1 =parseInt(num1.value());
-  n2 =parseInt(num2.value());
-  var n3 = n1/n2 
-  console.log(n3);
-}
-
-function sub() { 
-  n1 =parseInt(num1.value());
-  n2 =parseInt(num2.value());
-  var n3 = n1-n2 
-  console.log(n3);
+  background(r,g,b)
 }
